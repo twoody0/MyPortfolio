9536385fcb8917dc1976d6d50c16e0090b54243b
Windeko <tylerjameswoody@gmail.com>
2025-08-03 00:14:06 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    root: 'client',
     plugins: [
       vue(),
       tailwindcss(),
     ],
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
     server: {
         port: 58176,
     }
